d070b0ebaab22b51f084ad13da0f1bd274925e50
xkarina2197 <xkarina2197@gmail.com>
2025-10-31 18:01:39 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+# # 1
+for i in range(1,11):
+    print(i)
+
+# 2
+for i in range(10,0,-1):
+    print(i)
+
+# 3
+n = int(input("son kirit: "))
+
+for i in range(2,n,2 ):
+    print(i)
+
+# 4
+n = int(input("son kirit: "))
+
+for i in range(1, n, 2):
+    print(i)
+
+# 7
+for i in  range(1, 100):
+    if i % 3 == 0:
+        print(i)
+
+# 8
+for i in range(1, 100):
+    if i % 5 == 0:
+        print(i)
